87f787ad0a9564a1ea5de1c37d020a7100a4058b
David Nyström <david.c.nystrom@gmail.com>
2013-12-15 11:19:27 +0000

    Added building against a server
    Signed-off-by: David Nyström <david.c.nystrom@gmail.com>

--- add_machines.sh
+++ add_machines.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # $1 should be repo base directory
-#
+# $2 shoule be BBSERVER=localhost:8200 if a BBSERVER is used
 
 machines=$(find $1 -regextype posix-extended -regex '.*machine/.*\.conf' | xargs -i realpath {} | grep -v scripts | sed -e 's/.*\(machine\/.*\)\.conf/\1/g' | sed -e 's:machine/::g')
 echo  "#!/bin/bash" > buildall.sh
 
 for machine in $machines; do
-    echo "MACHINE=\"$machine\" bitbake core-image-minimal" >> buildall.sh
-    echo "SDKMACHINE=\"x86_64\" MACHINE=\"$machine\" bitbake -c populate_sdk core-image-minimal" >> buildall.sh
-    echo "SDKMACHINE=\"i686\" MACHINE=\"$machine\" bitbake -c populate_sdk core-image-minimal" >> buildall.sh
-    echo "MACHINE=\"$machine\" bitbake -k world" >> buildall.sh
+    echo "$2 MACHINE=\"$machine\" bitbake core-image-minimal" >> buildall.sh
+    echo "$2 SDKMACHINE=\"x86_64\" MACHINE=\"$machine\" bitbake -c populate_sdk core-image-minimal" >> buildall.sh
+    echo "$2 SDKMACHINE=\"i686\" MACHINE=\"$machine\" bitbake -c populate_sdk core-image-minimal" >> buildall.sh
+    echo "$2 MACHINE=\"$machine\" bitbake -k world" >> buildall.sh
 done
