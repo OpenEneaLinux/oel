0f53ace60656b5c60fa63d230f919cbdd94103b1
David Nyström <david.nystrom@enea.com>
2014-01-20 11:18:39 +0100

    Removed meta-eca layer
    Signed-off-by: David Nyström <david.nystrom@enea.com>

--- add_layers.sh
+++ add_layers.sh
@@ -3,7 +3,7 @@
 # $1 should be repo base directory
 #
 
-layers=$(find $1/meta* -name layer.conf | xargs -i realpath {}  | grep -v scripts | grep -v meta-tlk | grep -v obsolete | sed -e 's:/conf/layer.conf: \\:g')
+layers=$(find $1/meta* -name layer.conf | xargs -i realpath {}  | grep -v scripts | grep -v meta-tlk | grep -v 'meta-eca' | grep -v obsolete | sed -e 's:/conf/layer.conf: \\:g')
 bblayers=$(find $1 -name bblayers.conf)
 if [ -z $bblayers ]; then
     echo "Error: Please source the OE environment first"
